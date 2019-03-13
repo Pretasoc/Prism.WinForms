24411f761361fb98a622d0e74a41ad73f654d03d
Philemon Eichin <phileicht@gmx.de>
2019-03-13 08:00:37 +0100

    Add existing source

---
+++ Prism.WinForms/Mvvm/AutoWireViewModelAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prism.WinForms.Mvvm
+{
+    /// <summary>
+    /// Replaces the AutoWire Property
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+    public class AutoWireViewModelAttribute : Attribute
+    {
+        public AutoWireViewModelAttribute(bool autoWire = false)
+        {
+            AutoWire = autoWire;
+        }
+
+        public bool AutoWire { get; }
+    }
+}
